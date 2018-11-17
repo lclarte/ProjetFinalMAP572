69ed6001502111cdf81595e8f451155445d00e38
Lucas CLARTE <clarte.lucas@gmail.com>
2018-11-17 23:33:46 +0100

    cree la classe GestionnaireCalcul pour gerer le calcul des probas des degres

--- figures.py
+++ figures.py
@@ -3,53 +3,65 @@ np = core.np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-def estimation_probas_degres(N, M, show=False):
-	degres_sommets = np.zeros((N, N))
-	#matrice de taille N x N : l'element (i, j) est le nombre de graphes (entre 0 et M) dans lequel
-	#le sommet i a eu j aretes
-	for _ in range(M):
-		G = core.construire_G(N)
-		degres = np.sum(G, axis=0).astype(int)
-		for i in range(N):
-			d = degres[i]
-			degres_sommets[i, d] += 1.0
-	for i in range(N):
-		degres_sommets[i] = degres_sommets[i]/np.linalg.norm(degres_sommets[i])
-	probas_degres = np.average(degres_sommets, axis=0)
-	if show:
-		plt.plot(np.linspace(1, N, N), probas_degres)
-		plt.show()
-	return probas_degres
+"""
+Nota bene : si on a un linspace l , et qu'on veut selectionner une sous matrice a partir de l
+(i.e les colonnes dans l et lignes dans l), on fait M[l][:, l]
+"""
 
-def calcul_nombre_degres(N, M, show=False):
-	#on fait avec un seul graphe
-	nb_s_degres= [0]*N #nombre de sommets qui ont le degre s 
-	for _ in range(M):
-		G = core.construire_G(N)
-		degres = np.sum(G, axis=0).astype(int) #stocke les degres de chaque sommet 
-		for d in degres: #pour chaque sommet, on regarde son degre d 
-			nb_s_degres[d] += 1
-	nb_s_degres = np.array(nb_s_degres)
-	nb_s_degres = list(nb_s_degres/np.sum(nb_s_degres))
-	tmp = [(i, nb_s_degres[i]) for i in range(len(nb_s_degres)) if nb_s_degres[i] != 0]
-	X = np.array([t[0] for t in tmp])
-	Y = np.array([t[1] for t in tmp])
-	Y = [Y[i] for i in range(len(X))] #if X[i] < 50"""
-	X = [x for x in X] #if x < 50"""
-	if show:	
-		plt.plot(np.log(X), np.log(Y))
-		plt.show()
-	return np.log(X), np.log(Y)
+class GestionnaireCalcul:
+	#TODO : Implémenter la possibilité de continuer les simulations de Monte-Carlo
+	def __init__(self, N, M):
+		self.M, self.N = M, N
+		self.sommets_degres_comptes = None
+	
+	def estimation_sdc(self, verbose=False):
+		N, M = self.N, self.M
+		sommets_degres_comptes = np.zeros((N, N), dtype=int)
+		for _ in range(M):
+			if verbose and _ % 10 == 0:
+				print("Nombre de graphes calcules : ", _)
+			G = core.construire_G(N)
+			degres = np.sum(G, axis=0).astype(int)  #ici, on peut sommer selon l'axe 0 ou 1
+			#puisque la matrice est symetrique
+			for i in range(N):
+				d = degres[i]
+				sommets_degres_comptes[i, (d-1)] += 1
+		#degres_sommets[i, j] = nombre de fois que le sommet i a le degre j au total
+		#on l'enregistre en memoire 
+		self.sommets_degres_comptes = sommets_degres_comptes
+	
+	#todo : calculer la proba des degres
+	def calculer_probas(self):
+		#1) on calcule, pour chaque sommet, la proba que ce sommet soit d'un degre k :
+		#on divise simplement par M
+		try:
+			probas_degres_sommets = self.sommets_degres_comptes/float(self.M)
+			#on fait la moyenne sur les sommets qui sont les indices des lignes de la matrice
+			#donc on fait np.average(..., axis=0)
+			probas_degres = np.average(probas_degres_sommets, axis=0)
+			print(probas_degres)
+			return probas_degres
+		except Exception as e:
+			print("Erreur dans la matrice sommets_degres_comptes :", e)
 
-def regression(log_X, log_Y):
-	regr = LinearRegression()
-	regr.fit(log_Y, log_X)
-	return regr.coef_
+	def charger_sdc(self, fichier, M):
+		self.sommets_degres_comptes = np.load(fichier)
+		self.M = M
+
+	def sauvegarder_sdc(self, fichier):
+		np.save(fichier, self.sommets_degres_comptes)
+
+def afficher_log_log(probas):
+	M = len(probas)
+	X = np.log(np.linspace(1, M, M))
+	Y = np.log(probas)
+	plt.plot(X, Y)
+	plt.show()
+
 
 if __name__ == '__main__':
-	N, M = 5000, 500
-	probas_degres = estimation_probas_degres(N, M, show=False)
-	log_progas = np.log(probas_degres)
-	log_linspace = np.log(np.linspace(1, N, N))
-	plt.plot(log_linspace, log_progas)
-	plt.show()
+	N, M = 2000, 100
+	gc = GestionnaireCalcul(N, M)
+	gc.estimation_sdc(verbose=True)
+	probas = gc.calculer_probas()
+	afficher_log_log(probas)
