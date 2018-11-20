4e50c1bdfa1145a9279e0c42fada8d902d58f0be
Lucas CLARTE <clarte.lucas@gmail.com>
2018-11-20 16:02:48 +0100

    ajout du clustering fonctionnel

--- clustering.py
+++ clustering.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.linalg as linalg
+import sklearn.cluster as cluster
 from graph_display import *
+from core import *
 
 #Pour le graphe de similarite, on va prendre l'inverse de la matrice de distance (pour essayer) ainsi
 #que la matrice d'adjacence 
@@ -10,14 +12,34 @@ class ClusteringManager():
 		self.G = G
 		self.k = k
 
-	def calculer_similarite_fw(G):
-		distances = floyd_warshall(G)
+	def calculer_similarite_fw(self, G):
+		distances = floyd_warshall(G) + 1 #on ajoute un car pour un meme sommet, distances[i, i] = 0
+		#et l'inverse buguera
 		return (1/distances)
 
-	def calculer_laplacien(S):
+	def calculer_laplacien(self, S):
 		diagonale = np.sum(S, axis=0)
 		return (np.diag(diagonale) - S)
 
-	def calculer_k_eig(L):
-		w, vr = linalg.eig(L)
-		
+	def calculer_k_eig(self, L, k):
+		w, vr = linalg.eig(L) #note : vr contient les vecteurs propres en tant que colonnes
+		w     = np.real(w)
+		indexes = np.argsort(w)[:k] #on prend les k premieres vp
+		w, vr = w[indexes], np.array([l[indexes] for l in vr])
+		print("shape : ", vr.shape)
+		return w, vr #on extrait les k premieres lignes de vr
+
+	def clusters(self, vr, k):
+		kmeans = cluster.KMeans(n_clusters=k).fit(vr)
+		return kmeans.labels_
+
+	def calculer_clustering_G(self):
+		G, k = self.G, self.k
+		#S = self.calculer_similarite_fw(G)
+		S = G
+		L = self.calculer_laplacien(S)
+		w, vr = self.calculer_k_eig(L, k)
+		self.labels = self.clusters(vr, k)
+		ga = GestionnaireAffichage(G)
+		M = ga.calculer_affichage_optimise()
+		ga.afficher_points(M, debug=False, labels=self.labels)
