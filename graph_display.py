07263f414747402f7cfb5cc42e5bf7babc01c10a
lucas <clarte.lucas@gmail.com>
2018-11-26 15:19:24 +0100

    Ajout de l'affichage en 3A des graphes

--- graph_display.py
+++ graph_display.py
@@ -4,6 +4,20 @@ np.seterr(divide='ignore', invalid='ignore')
 import matplotlib.pyplot as plt
 import scipy.optimize as opti
 
+class GestionnaireAffichage3D(GestionnaireAffichage):
+	def __init__(self, G):
+		super(GestionnaireAffichage3D, self).__init__(G)
+
+	def calculer_points_affichage(self)
+		n = len(self.G)
+		X = np.random.uniform(size=n)
+		Y = np.random.uniform(size=n)
+		Z = np.random.uniform(size=n)
+		return np.array([x, y, z] for (x, y) in zip(X, Y, Z))
+
+	def afficher_points(self, ):
+		pass
+
 class GestionnaireAffichage:
 	def __init__(self, G):
 		self.G = G
@@ -16,7 +30,6 @@ class GestionnaireAffichage:
 				"; dt : " + str(self.delta_t)
 
 	def calculer_points_affichage(self):
-		plt.clf()
 		n = len(self.G)
 		X = np.random.uniform(size=n)
 		Y = np.random.uniform(size=n)
@@ -33,7 +46,10 @@ class GestionnaireAffichage:
 					plt.plot([X[i], X[j]], [Y[i], Y[j]], color='r')
 		plt.show()
 
-	def afficher_points(self, M, debug=True, D=None, labels=None):
+	def afficher_points(self, M, debug=True, D=None, labels=None, gradient=False):
+		"gradient = True, on va utiliser des gradients de couleur pour l'affichage : \
+		plus un sommet est bas, plus sa couleur sera foncee"
+		assert labels is None or not gradient
 		if labels is None:
 			labels = [0]*len(M)
 		colors = ['k', 'b', 'g', 'r', 'c', 'm', 'y', 'w']
@@ -61,7 +77,7 @@ class GestionnaireAffichage:
 		n = len(M)
 		grad_e_normes, energies = [], []
 		if method == 0:
-			M, grad_e_normes, energies = affichage_optimise_gradient(M, D_star)
+			M, grad_e_normes, energies = self.affichage_optimise_gradient(M, D_star)
 			if verbose:
 				plt.plot(np.linspace(1, len(energies), len(energies)), grad_e_normes)
 				plt.suptitle("Evolution du gradient de l'energie en fonction des iterations")
@@ -69,11 +85,14 @@ class GestionnaireAffichage:
 		elif method == 1:
 			res = opti.minimize(lambda m: energie_vec(m, D_star), vectoriser_M(M))
 			M = matriciser_M(res.x)
+			if not res.success:
+				raise Exception("La minimisation n'a pas convergé")
 		else:
 			raise Exception("Argument method incorrect")
 		return M
 
 	def affichage_optimise_gradient(self, M, D_star):
+		n = len(M)
 		gradient = None
 		grad_e_normes = []
 		energies = []
@@ -119,10 +138,8 @@ def calculer_gradient_energie(M, D_star):
 	tmp_vec = lambda vec, dist: [vec/dist, np.zeros((2,))][dist == 0.0]
 	tmp_coeffs = lambda x, dist: [x/dist, 0.0][dist == 0.0]
 	#ces deux fonctions permettent d'effectuer les operations avec numpy sans avoir des nan partout
-
 	n = len(M)
 	distances_M = np.array([[np.linalg.norm(M[i] - M[j]) for j in range(n)] for i in range(n)])
-
 	vecteurs_M  = np.array([[tmp_vec((M[i] - M[j]), distances_M[i, j]) for j in range(n)] for i in range(n)])
 	#vecteurs_= vecteurs directeurs des aretes entre chacun des points
 	coefficients = np.sqrt(0.5)*np.array([[tmp_coeffs(np.sqrt(0.5)*distances_M[i, j] - D_star[i, j], D_star[i, j]) \
@@ -130,6 +147,8 @@ def calculer_gradient_energie(M, D_star):
 	gradient_E = np.dot(coefficients, vecteurs_M)
 	gradient_E = np.array([gradient_E[i, i] for i in range(n)])
 	return gradient_E
+
+#Fonctions pour le calcul de la matrice D_star
 
 def floyd_warshall(G):
 	M, n = np.copy(G), len(G)
